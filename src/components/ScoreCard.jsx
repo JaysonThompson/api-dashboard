3f16f5705c531195198548668dbded82885e6767
Jayson Thompson <jaysonthompson922@gmail.com>
2023-08-01 15:08:09 -0700

    refactored

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 export default function ScoreCard() {
   const api =
     "https://statsapi.web.nhl.com/api/v1/schedule?teamId=55&startDate=2022-10-12&endDate=2023-04-13";
-  const scoreCardElement = document.querySelector(".score-cards-container");
   const [gameInfo, setGameInfo] = useState([]);
 
   const gameStatsArray = gameInfo.map((date) => {
@@ -25,37 +24,35 @@ export default function ScoreCard() {
       };
     });
   });
-  const scoreCards = gameStatsArray.map((game, index) => {
-    scoreCardElement.innerHTML += `
-      <div class="score-card">
-        <div class="score-card-content">
-          <header>
-            <h3>${game[0].date}</h3>
-            <p>Game ${index + 1}</p>
-          </header>
-          <div class="home team-info">
-            <h4 class="name">${game[0].home}</h4>
-            <div class="stats">
-              <p class="record">${game[0].homeWins}-${game[0].homeLosses}-${
-      game[0].homeOT
-    }</p>
-              <p class="score">${game[0].homeScore}</p>
-            </div> 
+  const scoreCardElement = gameStatsArray.map((game, index) => (
+    <div key={index + 1} className="score-card">
+      <div className="score-card-content">
+        <header>
+          <h3>{game[0].date}</h3>
+          <p>Game {index + 1}</p>
+        </header>
+        <div className="home team-info">
+          <h4 className="name">{game[0].home}</h4>
+          <div className="stats">
+            <p className="record">
+              {game[0].homeWins}-{game[0].homeLosses}-{game[0].homeOT}
+            </p>
+            <p className="score">{game[0].homeScore}</p>
           </div>
-          <div class="away team-info">
-            <h4 class="name">${game[0].away}</h4>
-            <div class="stats">
-              <p class="record">${game[0].awayWins}-${game[0].awayLosses}-${
-      game[0].awayOT
-    }</p>
-              <p class="score">${game[0].awayScore}</p>
-            </div>   
-          </div>
-          <p class="venue">Venue: ${game[0].venue}</p>  
         </div>
+        <div className="away team-info">
+          <h4 className="name">{game[0].away}</h4>
+          <div className="stats">
+            <p className="record">
+              {game[0].awayWins}-{game[0].awayLosses}-{game[0].awayOT}
+            </p>
+            <p className="score">{game[0].awayScore}</p>
+          </div>
+        </div>
+        <p className="venue">Venue: {game[0].venue}</p>
       </div>
-    `;
-  });
+    </div>
+  ));
 
   useEffect(() => {
     async function getKrakenData(apiUrl) {
@@ -74,5 +71,5 @@ export default function ScoreCard() {
     getKrakenData(api);
   }, []);
 
-  return <div className="score-cards-container">{scoreCards}</div>;
+  return <div className="score-cards-container">{scoreCardElement}</div>;
 }
