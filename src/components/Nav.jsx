972543c24a8ee94990dc13cea951b7ff3462f5a5
Jayson Thompson <jaysonthompson922@gmail.com>
2023-08-01 13:14:13 -0700

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,22 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Nav() {
+  const active = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#68a2b9",
+  };
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="schedule">Schedule</Link>
+      <NavLink to="/" style={({ isActive }) => (isActive ? active : null)}>
+        Home
+      </NavLink>
+      <NavLink
+        to="schedule"
+        style={({ isActive }) => (isActive ? active : null)}
+      >
+        Schedule
+      </NavLink>
     </nav>
   );
 }
