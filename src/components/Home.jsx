6e889d702c0fa58d76131ad1e76acf81c6cd1feb
Jayson Thompson <jaysonthompson922@gmail.com>
2023-08-08 10:58:52 -0700

    new 3D image for homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
+import "../3D.css";
+
 export default function Home() {
   return (
     <div className="main-container">
-      <h1>Home</h1>
+      <div className="card">
+        <div className="card-content">
+          <div className="card-front">
+            <h3 className="card-title">Seattle Kraken</h3>
+            <p className="card-subtitle">Release the KRAKEN</p>
+          </div>
+          <div className="card-back">
+            <p className="card-body">
+              Some really awesome Kraken information will go here
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
