97b8a10c5a2a10d35fdfa3009f9941ecca5bd5ed
Jayson Thompson <jaysonthompson922@gmail.com>
2023-08-10 12:12:48 -0700

    hero text animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from "react";
 import "./css/hero.css";
 
 export default function Hero({ title }) {
+  useEffect(() => {
+    const titleEl = document.querySelector(".hero-title");
+
+    setTimeout(() => {
+      titleEl.classList.add("animate");
+    }, 2000);
+  }, []);
+
   return (
     <div className="hero-text-wrapper">
-      <h1 className="hero-title">{title}</h1>
+      <h1 className="hero-title" data-text={title}>
+        {title}
+      </h1>
     </div>
   );
 }
